bb27f679958cb05e0303946ef063dbb7d025682f
mahmoudotman-123 <mahmoudetman6@gmail.com>
2024-03-20 14:26:13 +0200

    start server

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config({ path: "config.env" });
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`)
+);
