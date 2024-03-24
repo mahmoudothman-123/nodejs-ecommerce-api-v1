46c0c8efb46ddb2cdb54c812ae15577d11dc86fe
mahmoudothman-123 <mahmoudetman6@gmail.com>
2024-03-24 22:33:55 +0200

    add env and morgan

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+
+dotenv.config({ path: "config.env" });
+const app = express();
+
+const port = process.env.PORT;
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+  console.log(`you work in ${process.env.NODE_ENV} mode`);
+}
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.listen(port, () => {
+  console.log(`app is running in port ${port}`);
+});
